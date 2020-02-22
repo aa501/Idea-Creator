88538cb9f97f7640dbca138bd1b6003ed5f7a9c1
baileycash <baileycash012@gmail.com>
2020-02-22 09:28:05 -0800

    concept class

---
+++ MindOverMapper_Movim/MindOverMapper_Movim/Models/Concept.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MindOverMapper_Movim.Models
+{
+    public partial class Permissions
+    {
+      public int Id { get; set; }
+      public string Uid { get; set; }
+      public string ConceptName { get; set; }
+      public string NewsHeadline { get; set; }
+      public string Customer { get; set; }
+      public string CustomerProblem { get; set; }
+      public string Promise { get; set; }
+      public string Proof { get; set; }
+      public string Price { get; set; }
+      public string Passion { get; set; }
+      public string DeathThreats { get; set; }
+      public int ProjectId { get; set; }
+    }
+}
